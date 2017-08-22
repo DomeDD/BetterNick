3ff565b731f0b4bd62e636595edbd85a1a7d04b8
DomeDD <59269dd@gmail.com>
2017-08-22 22:51:04 +0200

    Update PlayerUnNickEvent.java

--- PlayerUnNickEvent.java
+++ PlayerUnNickEvent.java
@@ -1,42 +1,23 @@
 /*
  * All rights by DomeDD
- * You are allowed to modify this code
- * You are allowed to use this code in your plugins for private projects
- * You are allowed to publish your plugin including this code as long as your plugin is for free 
- * You are NOT allowed to claim this plugin as your own
- * You are NOT allowed to publish this plugin or your modified version of this plugin
- * 
  */
-package BetterNick.API.Events;
 
-import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
-import org.bukkit.event.HandlerList;
-
-import BetterNick.API.NickAPI;
-
-public class PlayerUnNickEvent extends Event {
+public class PlayerUnNickEvent {
 	
-	public static HandlerList handlers = new HandlerList();
-	private Player player;
+	/**
+   	 * @return player
+	 */
+	Player getPlayer();
 	
-	public PlayerUnNickEvent(Player arg0) {
-		player = arg0;
-	}
-	public Player getPlayer() {
-		return player;
-	}
-	public void setUnNickMessage(String arg0) {
-		player.sendMessage(arg0);
-	}
-	public void stopNickActionbarMessage() {
-		NickAPI.endActionBar(player.getUniqueId());
-	}
-	@Override
-	public HandlerList getHandlers() {
-		return handlers;
-	}
-	public static HandlerList getHandlerList() {
-		return handlers;
-	}
+	/**
+   	 * @return nickname
+	 */
+	String getNickName();
+	
+	/**
+   	 * @param arg0 message
+	 */
+	void setUnNickMessage(String arg0);
+	
+	void stopNickActionbarMessage();
 }
