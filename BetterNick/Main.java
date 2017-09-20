0e57eb200b6c8dae7d1eecab96fac7f91f5b41be
DomeDD <59269dd@gmail.com>
2017-09-20 19:21:31 +0200

    Add files via upload

--- BetterNick/Main.java
+++ BetterNick/Main.java
@@ -10,6 +10,7 @@
 package BetterNick;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
@@ -24,11 +25,14 @@ import BetterNick.API.Updater.SpigetUpdate;
 import BetterNick.API.Updater.UpdateCallback;
 import BetterNick.API.Updater.Comparator.VersionComparator;
 import BetterNick.CMD.NickCMD;
+import BetterNick.CMD.NickListCMD;
 import BetterNick.CMD.RealNameCMD;
 import BetterNick.CMD.SkinCMD;
 import BetterNick.CMD.UnNickCMD;
 import BetterNick.Files.NickedPlayers;
 import BetterNick.Listeners.AutoNick;
+import BetterNick.Listeners.BetterNickEvents;
+import BetterNick.Listeners.PlayerDeath;
 import BetterNick.MySQL.MySQL_Connection;
 import BetterNick.Versions.v1_10_R1;
 import BetterNick.Versions.v1_11_R1;
@@ -38,31 +42,18 @@ import BetterNick.Versions.v1_8_R3;
 import BetterNick.Versions.v1_9_R1;
 import BetterNick.Versions.v1_9_R2;
 import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.permission.Permission;
 
 public class Main extends JavaPlugin implements Listener {
 	
 	public Field nameField;
 	public Chat chat = null;
+	public Permission perms = null;
 	public Logger log = this.getLogger();
 	public ArrayList<String> nickedPlayers = new ArrayList<>();
 	
 	public void onEnable() {
-		if(getConfig().getBoolean("Config.API Mode")) {
-			loadAPIMode();
-		} else {
-			loadNormalMode();
-		}
 		checkandLoadVersions();
-		MySQL_Connection.setDefaultMySQL();
-		saveDefaultConfig();
-		if(getConfig().getBoolean("Config.MySQL")) {
-			MySQL_Connection.connectMySQL();
-	    	MySQL_Connection.connect();
-	    	MySQL_Connection.addTable();
-		} else {
-			NickedPlayers.loadDefaultFile();
-		}
-		checkForUpdates();
 	}
 	public void onDisable() {
 		MySQL_Connection.close();
@@ -104,27 +95,66 @@ public class Main extends JavaPlugin implements Listener {
 			});
 		}
 	}
-	private void loadAPIMode() {
-		nameField = getField(GameProfile.class, "name");
-		this.getServer().getPluginManager().registerEvents(new NickAPI(this), this);
-		this.getServer().getPluginManager().registerEvents(new MySQL_Connection(this), this);
-    	log.info("Plugin activated in API mode");
-	}
-	private void loadNormalMode() {
-		nameField = getField(GameProfile.class, "name");
-		this.getServer().getPluginManager().registerEvents(new NickAPI(this), this);
-		this.getServer().getPluginManager().registerEvents(new AutoNick(this), this);
-		this.getServer().getPluginManager().registerEvents(new MySQL_Connection(this), this);
-		getCommand("nick").setExecutor(new NickCMD(this));
-		getCommand("unnick").setExecutor(new UnNickCMD(this));
-		getCommand("realname").setExecutor(new RealNameCMD(this));
-		getCommand("skin").setExecutor(new SkinCMD());
-    	if(getConfig().getBoolean("Config.Use Vault")) {
-    		RegisteredServiceProvider<Chat> chatProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.chat.Chat.class);
-            if (chatProvider != null) {
-                chat = chatProvider.getProvider();
-            }
-    	}
+	private void loadPlugin() {
+		if(getConfig().getBoolean("Config.API Mode")) {
+			nameField = getField(GameProfile.class, "name");
+			this.getServer().getPluginManager().registerEvents(new NickAPI(this), this);
+			this.getServer().getPluginManager().registerEvents(new PlayerDeath(this), this);
+			this.getServer().getPluginManager().registerEvents(new MySQL_Connection(this), this);
+			if(getConfig().getBoolean("Config.Use Vault")) {
+	    		RegisteredServiceProvider<Chat> chatProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.chat.Chat.class);
+	    		RegisteredServiceProvider<Permission> permsProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
+	            if (chatProvider != null) {
+	                chat = chatProvider.getProvider();
+	            }
+	            if (permsProvider != null) {
+	                perms = permsProvider.getProvider();
+	            }
+	    	}
+			MySQL_Connection.setDefaultMySQL();
+			saveDefaultConfig();
+			if(getConfig().getBoolean("Config.MySQL")) {
+				MySQL_Connection.connectMySQL();
+		    	MySQL_Connection.connect();
+		    	MySQL_Connection.addTable();
+			} else {
+				NickedPlayers.loadDefaultFile();
+			}
+			checkForUpdates();
+	    	log.info("Plugin activated in API mode");
+		} else {
+			nameField = getField(GameProfile.class, "name");
+			this.getServer().getPluginManager().registerEvents(new NickAPI(this), this);
+			this.getServer().getPluginManager().registerEvents(new AutoNick(this), this);
+			this.getServer().getPluginManager().registerEvents(new PlayerDeath(this), this);
+			this.getServer().getPluginManager().registerEvents(new MySQL_Connection(this), this);
+			this.getServer().getPluginManager().registerEvents(new BetterNickEvents(this), this);
+			getCommand("nick").setExecutor(new NickCMD(this));
+			getCommand("unnick").setExecutor(new UnNickCMD(this));
+			getCommand("realname").setExecutor(new RealNameCMD(this));
+			getCommand("skin").setExecutor(new SkinCMD());
+			getCommand("nicklist").setExecutor(new NickListCMD(this));
+	    	if(getConfig().getBoolean("Config.Use Vault")) {
+	    		RegisteredServiceProvider<Chat> chatProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.chat.Chat.class);
+	    		RegisteredServiceProvider<Permission> permsProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
+	            if (chatProvider != null) {
+	                chat = chatProvider.getProvider();
+	            }
+	            if (permsProvider != null) {
+	                perms = permsProvider.getProvider();
+	            }
+	    	}
+	    	MySQL_Connection.setDefaultMySQL();
+			saveDefaultConfig();
+			if(getConfig().getBoolean("Config.MySQL")) {
+				MySQL_Connection.connectMySQL();
+		    	MySQL_Connection.connect();
+		    	MySQL_Connection.addTable();
+			} else {
+				NickedPlayers.loadDefaultFile();
+			}
+			checkForUpdates();
+		}
 	}
 	private void checkandLoadVersions() {
 		if(Bukkit.getVersion().contains("(MC: 1.8)") || Bukkit.getVersion().contains("(MC: 1.8.1)") || Bukkit.getVersion().contains("(MC: 1.8.2)")) {
@@ -132,25 +162,32 @@ public class Main extends JavaPlugin implements Listener {
 			return;
 		} else if(Bukkit.getVersion().contains("(MC: 1.8.3)")) {
 			this.getServer().getPluginManager().registerEvents(new v1_8_R2(this), this);
-			log.info("Hooking into v1_8_R2");
+			loadPlugin();
+			log.info("Hooked into v1_8_R2 successfully");
 		} else if(Bukkit.getVersion().contains("(MC: 1.8.4)") || Bukkit.getVersion().contains("(MC: 1.8.5)") || Bukkit.getVersion().contains("(MC: 1.8.6)") || Bukkit.getVersion().contains("(MC: 1.8.7)") || Bukkit.getVersion().contains("(MC: 1.8.8)")) {
 			this.getServer().getPluginManager().registerEvents(new v1_8_R3(this), this);
-			log.info("Hooking into v1_8_R3");
+			loadPlugin();
+			log.info("Hooked into v1_9_R1 successfully");
 		} else if(Bukkit.getVersion().contains("(MC: 1.9)") || Bukkit.getVersion().contains("(MC: 1.9.1)") || Bukkit.getVersion().contains("(MC: 1.9.2)") || Bukkit.getVersion().contains("(MC: 1.9.3)")) {
 			this.getServer().getPluginManager().registerEvents(new v1_9_R1(this), this);
-			log.info("Hooking into v1_9_R1");
+			loadPlugin();
+			log.info("Hooked into v1_9_R2 successfully");
 		} else if(Bukkit.getVersion().contains("(MC: 1.9.4)")) {
 			this.getServer().getPluginManager().registerEvents(new v1_9_R2(this), this);
-			log.info("Hooking into v1_9_R2");
+			loadPlugin();
+			log.info("Hooked into v1_9_R3 successfully");
 		} else if(Bukkit.getVersion().contains("(MC: 1.10)") || Bukkit.getVersion().contains("(MC: 1.10.1)") || Bukkit.getVersion().contains("(MC: 1.10.2)")) {
 			this.getServer().getPluginManager().registerEvents(new v1_10_R1(this), this);
-			log.info("Hooking into v1_10_R1");
+			loadPlugin();
+			log.info("Hooked into v1_10_R1 successfully");
 		} else if(Bukkit.getVersion().contains("(MC: 1.11)") || Bukkit.getVersion().contains("(MC: 1.11.1)") || Bukkit.getVersion().contains("(MC: 1.11.2)")) {
 			this.getServer().getPluginManager().registerEvents(new v1_11_R1(this), this);
-			log.info("Hooking into v1_11_R1");
-		} else if(Bukkit.getVersion().contains("(MC: 1.12)")) {
+			loadPlugin();
+			log.info("Hooked into v1_11_R1 successfully");
+		} else if(Bukkit.getVersion().contains("(MC: 1.12)") || Bukkit.getVersion().contains("(MC: 1.12.1)") || Bukkit.getVersion().contains("(MC: 1.12.2)")) {
 			this.getServer().getPluginManager().registerEvents(new v1_12_R1(this), this);
-			log.info("Hooking into v1_12_R1");
+			loadPlugin();
+			log.info("Hooked into v1_12_R1 successfully");
 		} else {
 			log.warning("Your minecraft version is not supported");
 			return;
