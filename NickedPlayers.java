0fb1e89ef6cecbe0714c3c8a23b399da7a796d2e
DomeDD <59269dd@gmail.com>
2017-12-22 17:48:00 +0100

    Add files via upload

---
+++ NickedPlayers.java
@@ -0,0 +1,21 @@
+/*
+ * All rights by DomeDD
+ * You are allowed to modify this code
+ * You are allowed to use this code in your plugins for private projects
+ * You are allowed to publish your plugin including this code as long as your plugin is for free 
+ * You are NOT allowed to claim this plugin as your own
+ * You are NOT allowed to publish this plugin or your modified version of this plugin
+ * 
+ */
+class NickedPlayers implements Listener {
+	
+	/**
+	 * constructor
+	 */
+	NickedPlayers();
+	
+	/**
+	 * @return List of NickedPlayers
+	 */
+	List<NickedPlayer> getAll();
+}
